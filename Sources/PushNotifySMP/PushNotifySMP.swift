e338d7b9f994f21076d0ca9a3526224f97438b4a
Mikhail <wowivan2011@yandex.ru>
2021-04-16 13:57:00 +0300

    init project

--- Sources/PushNotifySMP/PushNotifySMP.swift
+++ Sources/PushNotifySMP/PushNotifySMP.swift
@@ -1,3 +1,20 @@
-struct PushNotifySMP {
-    var text = "Hello, World!"
+import SwiftUI
+import UserNotifications
+
+public func NotificationActivate() {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+        print("Permission granted: \(granted)")
+        guard granted else { return }
+        
+        getNotificationSettings()
+    }
+}
+
+fileprivate func getNotificationSettings() {
+    UNUserNotificationCenter.current().getNotificationSettings { settings in
+        guard settings.authorizationStatus == .authorized else { return }
+        DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+    }
 }
--- Tests/PushNotifySMPTests/PushNotifySMPTests.swift
+++ Tests/PushNotifySMPTests/PushNotifySMPTests.swift
@@ -2,14 +2,5 @@ import XCTest
 @testable import PushNotifySMP
 
 final class PushNotifySMPTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(PushNotifySMP().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
