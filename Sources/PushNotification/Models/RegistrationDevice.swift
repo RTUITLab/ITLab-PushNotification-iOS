16bf3dcddb1c53f7b7c55857a99eba586fc9e5e2
Mikhail <wowivan2011@yandex.ru>
2021-04-16 17:54:32 +0300

    Added registration device

---
+++ Sources/PushNotification/Models/RegistrationDevice.swift
@@ -0,0 +1,118 @@
+//
+//  RegistrationDevice.swift
+//  
+//
+//  Created by Mikhail Ivanov on 16.04.2021.
+//
+
+import Foundation
+
+struct RegistrationDeviceNone: Codable {
+    var device: DeviceInfo
+    
+    public init(device: String) {
+        self.device = .init(deviceID: device)
+    }
+    
+    func request(_ url: URL) {
+        let url = url.appendingPathComponent("/user/addDevice")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let json = try? JSONEncoder().encode(self) as Data
+        request.httpBody = json
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode != 200 {
+                    print("Not registration device")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct RegistrationDeviceJWT: Codable {
+    var device: DeviceInfo
+    var token: String
+    
+    public init(device: String, token: String) {
+        self.device = .init(deviceID: device)
+        self.token = token
+    }
+    
+    func request(_ url: URL) {
+        let url = url.appendingPathComponent("/user/addDevice")
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let json = try? JSONEncoder().encode(self) as Data
+        request.httpBody = json
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode != 200 {
+                    print("Not registration device")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    enum CodingKeys: CodingKey {
+        case device, token
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(device, forKey: .device)
+    }
+}
+
+struct RegistrationDeviceUser: Codable {
+    var device: DeviceInfo
+    var id: UUID
+    
+    public init(device: String, id: UUID) {
+        self.device = .init(deviceID: device)
+        self.id = id
+    }
+    
+    func request(_ url: URL) {
+        let url = url.appendingPathComponent("/user/addDevice")
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let json = try? JSONEncoder().encode(self) as Data
+        request.httpBody = json
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode != 200 {
+                    print("Not registration device")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+
+struct DeviceInfo: Codable {
+    let deviceID: String
+    let type: String
+    
+    init(deviceID: String) {
+        self.type = "ios"
+        self.deviceID = deviceID
+    }
+}
---
+++ Sources/PushNotification/PushNotification.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+import UIKit
+import UserNotifications
+
+public class PushNotification {
+    
+    private var authenticationMethod: AuthenticationMethod
+    private var url: URL
+    private static var shared: PushNotification?
+    
+    public enum AuthenticationMethod {
+        case user(id: UUID)
+        case jwt(token: String)
+        case none
+    }
+    
+    private init(authenticationMethod: AuthenticationMethod, url: URL) {
+        self.authenticationMethod = authenticationMethod
+        self.url = url
+    }
+    
+    public static func notificationActivate(_ url: String, authenticationMethod: AuthenticationMethod) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+            
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                guard settings.authorizationStatus == .authorized else { return }
+                
+                guard let url = URL(string: url) else {
+                    return
+                }
+                
+                shared = PushNotification(authenticationMethod: authenticationMethod, url: url)
+                
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                }
+            }
+        }
+    }
+    
+    public static func registrateDevice(device token: Data) {
+        
+        guard shared != nil else {
+            return
+            
+        }
+        
+        let tokenParts = token.map { data in String(format: "%02.2hhx", data) }
+        let deviceToken = tokenParts.joined()
+        
+        switch shared!.authenticationMethod {
+        case .jwt(let token):
+            RegistrationDeviceJWT(device: deviceToken, token: token).request(shared!.url)
+            break
+            
+        case .user(let id):
+            RegistrationDeviceUser(device: deviceToken, id: id).request(shared!.url)
+            break
+        
+        case .none:
+            RegistrationDeviceNone(device: deviceToken).request(shared!.url)
+            break
+        }
+    }
+}
